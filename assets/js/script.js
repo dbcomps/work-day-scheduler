ef7c6a3548e320bdcce1733a1da55a7c47d34ab9
David Barraza <dbcomps@mac.com>
2022-08-17 16:15:58 -0500

    add class past, future, present to color code time-blocks

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,17 +3,23 @@ console.log(currentDayTime);
 
 $("#currentDay").html(currentDayTime);
 
+// this uses 24 hours so time block need to be id in 24 hours
 var currentHour = moment().hour();
 console.log(currentHour);
 
 $(".time-block").each(function() {
 	// go through all the time-block class elements and get the hour for that time block
-	// added an id to uniquely identify each time-block
-	var hour = parseInt($(this).attr("id"));
-	console.log($(this).attr("id"));
+	// added an id to uniquely identify each time-block and convert to integer
+	var hourBlock = parseInt($(this).attr("id"));
+	console.log(hourBlock);
 	
-	console.log(hour);
-	
-	
-	
+	if (hourBlock < currentHour) {
+		$(this).addClass("past");
+	}
+	else if (hourBlock > currentHour) {
+		$(this).addClass("future");
+	}
+	else {
+		$(this).addClass("present");
+	}
 })
