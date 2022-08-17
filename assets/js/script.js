e348dbc57719adff4eb40f7bc65873da8b492c92
David Barraza <dbcomps@mac.com>
2022-08-17 16:03:32 -0500

    add hour id to each time-block to uniquely id know that hour

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,18 @@ var currentDayTime = moment().format('MMMM Do YYYY');
 console.log(currentDayTime);
 
 $("#currentDay").html(currentDayTime);
+
+var currentHour = moment().hour();
+console.log(currentHour);
+
+$(".time-block").each(function() {
+	// go through all the time-block class elements and get the hour for that time block
+	// added an id to uniquely identify each time-block
+	var hour = parseInt($(this).attr("id"));
+	console.log($(this).attr("id"));
+	
+	console.log(hour);
+	
+	
+	
+})
